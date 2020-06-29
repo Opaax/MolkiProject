3ad512ec75f9ec245f65b069effb7f99363f3046
EnguerranCobert <e.cobert@student.isartdigital.com>
2020-06-29 23:16:53 +0200

    init project

---
+++ MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/ScreenObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class ScreenObject : MonoBehaviour
+{
+    [Header("animation")]
+    [SerializeField] Animator animator = null;
+}
