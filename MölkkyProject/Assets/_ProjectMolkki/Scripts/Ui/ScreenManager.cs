ab748691637f3b488d62e4ea584109a3ec269415
EnguerranCobert <e.cobert@student.isartdigital.com>
2020-07-01 17:54:19 +0200

    add anime

---
+++ MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/ScreenManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager
+{
+    private List<ScreenObject> _currentScreenOpen = new List<ScreenObject>(); 
+
+    public static List<ScreenObject> allScreen = new List<ScreenObject>();
+
+    public List<ScreenObject> CurrentScreenOpen { get => _currentScreenOpen; }
+
+    public void Init()
+    {
+        InitScreens();
+    }
+
+    private void InitScreens()
+    {
+        for (int i = allScreen.Count - 1; i >= 0; i--)
+        {
+            allScreen[i].Init();
+        }
+    }
+}
--- MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/ScreenObject.cs
+++ MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/ScreenObject.cs
@@ -2,9 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animator),typeof(CanvasGroup),typeof(Canvas))]
 public class ScreenObject : MonoBehaviour
 {
-    [Header("animation")]
+    [Header("Animation")]
     [SerializeField] Animator animator = null;
+    [Space]
+    [Header("Canvas")]
+    [SerializeField] CanvasGroup canvasGroup = null;
+    [SerializeField] Canvas canvas = null;
+
+    private RectTransform rectTrans = null;
+
+    private void Awake()
+    {
+        ScreenManager.allScreen.Add(this);
+    }
+
+    public void Init()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+
+        rectTrans = GetComponent<RectTransform>();
+
+        SetToCanvas(); 
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    protected virtual void SetToCanvas()
+    {
+        rectTrans.offsetMin = Vector2.zero;
+        rectTrans.offsetMax = Vector2.zero;
+
+        rectTrans.anchorMin = Vector2.zero;
+        rectTrans.anchorMax = Vector2.one;
+    }
 }
