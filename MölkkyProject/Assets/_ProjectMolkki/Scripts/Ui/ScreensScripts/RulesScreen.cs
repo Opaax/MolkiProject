70340d8f16bba25c063d26df5a3084638caccbda
EnguerranCobert <e.cobert@student.isartdigital.com>
2020-07-06 14:22:25 +0200

    Add Scrolling

--- MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/ScreensScripts/RulesScreen.cs
+++ MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/ScreensScripts/RulesScreen.cs
@@ -8,4 +8,13 @@ public class RulesScreen : ScreenObject
     [Space]
     [Header("Button")]
     [SerializeField] private Button buttonBack = null;
+    [Space]
+    [Header("ScrollingArea")]
+    [SerializeField] private RectTransform scrollingContent = null;
+
+    public override void EndDisappear()
+    {
+        base.EndDisappear();
+        scrollingContent.position = new Vector3(scrollingContent.position.x, 0);
+    }
 }
