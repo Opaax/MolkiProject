b76768f05f59aa1542a14449b85ade7aa1d051f7
Enguerran Cobert <enguerran.c@amusenetwork.com>
2021-02-10 15:06:53 +0100

    Modif screen SoloGame

---
+++ MölkkyProject/Assets/_ProjectMolkki/Scripts/Ui/SoloGameScript/PlayerInfoBanner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerInfoBanner : MonoBehaviour
+{
+    [Header("TextInfos")]
+    [SerializeField] private TextMeshPro nameText = null;
+    [SerializeField] private TextMeshPro scoreText = null;
+
+    private void InitBannerInfo(string nameInfo, string scoreInfo)
+    {
+        nameText.text = nameInfo;
+        scoreText.text = scoreInfo;
+    }
+}
